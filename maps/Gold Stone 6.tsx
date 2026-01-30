b9c39054604f5f8f894f2a75a862962ad3dd2fb2
dashushker <dashushker@gmail.com>
2026-01-30 13:51:23 +0300

    файлики с картинками

---
+++ maps/Gold Stone 6.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Gold Stone 6" tilewidth="32" tileheight="32" tilecount="16" columns="4">
+ <image source="../resources/Tiny Swords (Free Pack)/Terrain/Resources/Gold/Gold Stones/Gold Stone 6.png" width="128" height="128"/>
+</tileset>
